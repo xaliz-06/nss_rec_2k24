8caede1963135ded437a2a2a2f2a4e71030cc761
xaliz-06 <vaibhavchakraborty06@gmail.com>
2024-08-21 20:52:48 +0530

    fixed favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/lakshya_hero.png" sizes="any" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
