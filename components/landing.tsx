a494fe64fb87075ecf9fa315b284fddf2c6b5f85
Aaditya Aanand <157479987+Jerry2603s@users.noreply.github.com>
2025-08-26 12:34:55 +0530

    Update landing.tsx

--- components/landing.tsx
+++ components/landing.tsx
@@ -44,7 +44,7 @@ const Landing = () => {
           </h2>
         </div>
         <h3 className="font-semibold text-xl tracking-tighter text-white md:text-3xl">
-          Result out soon!
+          Result is Out!
         </h3>
         <div className="w-full flex justify-center items-center">
           <Image
@@ -57,16 +57,16 @@ const Landing = () => {
 
         <div>
           <div className="p-2">
-            <Link href="" target="_blank">
-              {/* <Button
+            <Link href="https://www.instagram.com/p/DNzudjbZNgf/?igsh=MXZodDJ4Nm1qYWpvbA==" target="_blank">
+              { <Button
                 variant="default"
                 size="lg"
                 className="font-bold bg-[#070F2B] hover:bg-blue-600/20 hover:text-white text-white border-none outline-none focus-visible:ring-offset-0 focus-visible:ring-transparent focus:bg-blue-600/30 w-72 rounded-md transition px-8 py-6"
               >
                 <p className="font-bold text-white tracking-tight text-2xl">
-                  Register Closed!
+                  SEE RESULT!
                 </p>
-              </Button> */}
+              </Button> }
             </Link>
           </div>
         </div>
